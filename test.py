65523f93ffad9bbcf655b6f9ebe4a804584329d8
Sergey Gubar <sergey.gubar@sigma.software>
2019-05-17 16:03:55 +0300

    Implement sample recognition

--- test.py
+++ test.py
@@ -1,85 +1,41 @@
-import requests
-import os
-import time
-
-base_url = "http://f0fc465d.ngrok.io/api"
-token = None
-delay = 8
-
-
-def sign_in(email, password):
-    print("Logging in user " + email + password)
-
-    login_data = {
-        "email": email,
-        "password": password,
-        "rememberMe": "true"
-    }
-    login_url = base_url + "/User/login"
-    try:
-        r = requests.post(login_url, json=login_data)
-    except:
-        print("Internet is not available! Exiting..")
-        return
-
-    global token
-    token = r.json().get('access_token')
-    print(token)
+# import cv2
+# im_gray = cv2.imread("detail.jpg", cv2.IMREAD_GRAYSCALE)
+#
+# cv2.imshow("test", im_gray)
+#
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
-def upload_photo(token, file_name):
-    print("Uploading " + file_name)
-    if token is None:
-        print("Haven't got any valid token! You should login first")
-        return
-    headers = {'Authorization': 'Bearer ' + token}
-    url = base_url + "/user/uploadPhoto"
-    try:
-        file = open(file_name, "rb")
-    except FileNotFoundError:
-        print("File was not found!")
-        return
-
-    photo = {
-        "file": file
-    }
-    requests.post(url, files=photo, headers=headers)
-    print("Upload success!")
-    print("Removing file...")
-    os.remove(file_name)
-
-def get_all_files():
-    return list([f for f in os.listdir('.') if os.path.isfile(f)])
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
 
 
-def get_all_photos():
-    return list(filter(lambda x: x.endswith("png"), get_all_files()))
+def to_inches(sm):
+    return sm / 2.54
 
 
-def print_all_images():
-    files = get_all_files()
-    for f in files:
-        if f.endswith(".png"):
-            print(f)
+image_width = to_inches(19)
+image_height = to_inches(15)
+camera_paper_height = to_inches(13.3)
+reference_width = to_inches(1.5)
+reference_height = to_inches(1)
 
+img = cv2.imread('detail.jpg', 0)
+height_px, width_px = img.shape[:2]
+reference_width_px = int(width_px * (reference_width / image_width))
+pixel_per_metric = int(reference_width_px / reference_width)
 
-def upload_all_photos():
-    photos = get_all_photos()
-    if len(photos) == 0:
-        print("Oops! Photos are empty, nothing to download")
-        return
-    for photo in photos:
-        upload_photo(token, photo)
-
-
-config = open("config.json")
-text = config.read()
-print(text)
-
-sign_in(text.split()[0], text.split()[1])
-
-while True:
-    upload_all_photos()
-    time.sleep(delay)
-
-
+print("Original width (px) = " + str(width_px))
+print("Original height  (px) = " + str(height_px))
+print("Reference witdh (px) = " + str(reference_width_px))
+print("Pixel metric (px) = " + str(pixel_per_metric))
+# edges = cv2.Canny(img, 200, 500)
+#
+# plt.subplot(121), plt.imshow(img, cmap='gray')
+# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122), plt.imshow(edges, cmap='gray')
+# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+#
+# plt.show()
